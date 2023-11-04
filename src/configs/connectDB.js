c6ab0e7c797831a0550d9f7a81f57de38d4b81b3
truong <nthanhtruong883@gmail.com>
2023-11-04 15:52:36 +0700

    last

--- src/configs/connectDB.js
+++ src/configs/connectDB.js
@@ -2,10 +2,7 @@ import mongoose, { connect } from 'mongoose';
 require('dotenv').config();
 let connectDB = async() => {
   try {
-    let connect = await mongoose.connect(process.env.MONGOOSE_URL,{
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    let connect = await mongoose.connect(process.env.MONGOOSE_URL);
     if(connect.connection.readyState === 1) {
         console.log('Successfully connected MongoDB')
     }
